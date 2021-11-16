e1fe14df536225be244da34bedd57f5368669d37
rishiphatak <rishi.phatak1@gmail.com>
2021-11-16 09:35:08 -0600

    make helper generic

--- helper.py
+++ helper.py
@@ -5,77 +5,67 @@ Created on Mon Nov 15 15:44:57 2021
 @author: Rishi2
 """
 
-import json
+def generateConvJson(desc_file_name, basic_graph, to_file_name):
 
-length_desc = {
+    import json
     
-    "ft": "feet",
-    "in": "inches",
-    "cm": "centimeters",
-    "m": "meters",
-    "km": "kilometers"
-    }
-
-length_units = length_desc.keys()
-
-graph = {}
-
-for un in length_units:
-    graph[un] = {}
+    length_desc = json.load(open(desc_file_name))
     
-graph['ft']['in'] = 12
-graph['in']['cm'] = 2.54
-graph['m']['cm'] = 100
-graph['km']['m'] = 1000
-
-# make the edges bidirectional
-for unit in graph:
-    for other_unit in graph[unit]:
-        if unit not in graph[other_unit]:
-            graph[other_unit][unit] = 1/graph[unit][other_unit]
-            
-# convert any two units
-def convert(unit_1, unit_2, length_units, graph):
-    units = [unit_1]
-    visited = {}
-    parent = {}
-    for unit in length_units:
-        visited[unit] = False
-        parent[unit] = None
-    while len(units) != 0:
-        curr = units[0]; units = units[1:]
-        neighbors = graph[curr].keys()
-        for other_u in neighbors:
-            if not visited[other_u]:
-                units.append(other_u)
-                parent[other_u] = curr
-                visited[other_u] = True
-    prevPar = unit_2
-    currPar = parent[prevPar]
-    invRet = 1
-    while currPar != unit_1:
-        invRet *= graph[prevPar][currPar]
-        prevPar = currPar
-        currPar = parent[currPar]
-    invRet *= graph[prevPar][unit_1]
-    return 1/invRet
-
-other_gr = {}
-
-for un in length_units:
-    other_gr[un] = {}
-    for other_un in length_units:
-        if un == other_un:
-            other_gr[un][un] = 1
-        else:
-            conversion = convert(un, other_un, length_units, graph)
-            other_gr[un][other_un] = round(conversion, 3)
-print(other_gr)
-
-f = open("len_conv.json", "w")
-json.dump(other_gr, f)
-f.close()
-
-f = open("len_desc.json", "w")
-json.dump(length_desc, f)
-f.close()
+    length_units = length_desc.keys()
+    
+    graph = {}
+    
+    for un in length_units:
+        graph[un] = {}
+        
+    for key in basic_graph:
+        from_unit, to_unit = key.split("->")
+        graph[from_unit][to_unit] = basic_graph[key]
+    
+    # make the edges bidirectional
+    for unit in graph:
+        for other_unit in graph[unit]:
+            if unit not in graph[other_unit]:
+                graph[other_unit][unit] = 1/graph[unit][other_unit]
+                
+    # convert any two units
+    def convert(unit_1, unit_2, length_units, graph):
+        units = [unit_1]
+        visited = {}
+        parent = {}
+        for unit in length_units:
+            visited[unit] = False
+            parent[unit] = None
+        while len(units) != 0:
+            curr = units[0]; units = units[1:]
+            neighbors = graph[curr].keys()
+            for other_u in neighbors:
+                if not visited[other_u]:
+                    units.append(other_u)
+                    parent[other_u] = curr
+                    visited[other_u] = True
+        prevPar = unit_2
+        currPar = parent[prevPar]
+        invRet = 1
+        while currPar != unit_1:
+            invRet *= graph[prevPar][currPar]
+            prevPar = currPar
+            currPar = parent[currPar]
+        invRet *= graph[prevPar][unit_1]
+        return 1/invRet
+    
+    other_gr = {}
+    
+    for un in length_units:
+        other_gr[un] = {}
+        for other_un in length_units:
+            if un == other_un:
+                other_gr[un][un] = 1
+            else:
+                conversion = convert(un, other_un, length_units, graph)
+                other_gr[un][other_un] = round(conversion, 10)
+    print(other_gr)
+    
+    f = open(to_file_name, "w")
+    json.dump(other_gr, f)
+    f.close()
